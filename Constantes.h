e082217d07b51c897209dd0a37c39ff88039567e
oswald <omaldonado@fi.uba.ar>
2023-11-01 14:12:27 -0300

    agrego el archivo de constantes

---
+++ Constantes.h
@@ -0,0 +1,18 @@
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
+
+#define FILAS 20
+
+#define COLUMNAS 20
+
+#define ESPIA 'E'
+
+#define RECUPERANDO 'R'
+
+#define RECUPERANDO_CASILLA 5
+
+#define VACIO ' '
+
+#define TESORO 'X'
+
+#endif /* CONSTANTES_H_ */
