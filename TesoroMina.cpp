7d7e1f44d66702869fd24e0fc435372fa215888f
oswald1511 <omaldonado@fi.uba.ar>
2023-11-05 02:02:03 -0300

    agregue el constructor del tesoro mina

--- TesoroMina.cpp
+++ TesoroMina.cpp
@@ -1,7 +1,12 @@
 #include "TesoroMina.h"
+#include "Vector.h"
 
-TesoroMina::TesoroMina(int x, int y, int z){
+TesoroMina::TesoroMina(Vector<int> *posicion){
 
+	this->posicion = new Vector<int>(3);
+	this->posicion->set(1, posicion->get(1));
+	this->posicion->set(2, posicion->get(2));
+	this->posicion->set(3, posicion->get(3));
 }
 
 TesoroMina::~TesoroMina() {
