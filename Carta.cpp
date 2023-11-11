37c6803900e5dbfc0ca99cda51f0475eb8ce5065
Valeria G <84480384+Vxleriagreyna@users.noreply.github.com>
2023-11-10 22:32:02 -0300

    Update Carta.cpp
    comentariuo ladron y snipper

--- Carta.cpp
+++ Carta.cpp
@@ -16,7 +16,55 @@ Carta::~Carta(){
 void Carta::jugarCarta(){
 	//con switch(this->tipoCarta) estableccer lo que hace en este metodo cada una
 }
+/* void Carta::jugarCartaSniper(Jugador* jugadorActual, Jugadores* jugadores) {
+    int cantidadJugadores = jugadores->obtenerCantidadTotalDeJugadores();
+    
+    if (cantidadJugadores > 1) {
+        int jugadorAleatorio;
+        do {
+            jugadorAleatorio = rand() % cantidadJugadores;
+        } while (jugadorAleatorio == jugadorActual->getNumeroJugador());
 
+        Jugador* otroJugador = jugadores->obtenerJugadorPorNumero(jugadorAleatorio);
+
+        if (otroJugador != nullptr) {
+            // Obtén la lista de tesoros del otro jugador.
+            Lista<Tesoro*>* tesoros = otroJugador->getTesoros();
+
+            if (tesoros->contarElementos() > 0) {
+                // Elige al azar un tesoro del otro jugador para eliminar.
+                int indiceAleatorio = rand() % tesoros->contarElementos();
+                Tesoro* tesoroAEliminar = tesoros->obtenerElementoEnPosicion(indiceAleatorio);
+
+                // Elimina el tesoro del otro jugador.
+                otroJugador->getTablero()->removerTesoro(tesoroAEliminar);
+                tesoros->eliminar(tesoroAEliminar);
+            }
+        }
+    }
+
+
+
+void Carta::jugarCartaLadron(Jugador* jugadorActual, Jugadores* jugadores) {
+    int cantidadJugadores = jugadores->obtenerCantidadTotalDeJugadores();
+    if (cantidadJugadores > 1) {
+        int jugadorAleatorio;
+        do {
+            jugadorAleatorio = rand() % cantidadJugadores;
+        } while (jugadorAleatorio == jugadorActual->getNumeroJugador());
+
+        Jugador* otroJugador = jugadores->obtenerJugadorPorNumero(jugadorAleatorio);
+
+        if (otroJugador != nullptr && otroJugador->getCartas()->contarElementos() > 0) {
+            Carta* cartaRobada = otroJugador->getCartas()->obtenerElementoAleatorio();
+            jugadorActual->getCartas()->agregar(cartaRobada);
+            otroJugador->getCartas()->eliminar(cartaRobada);
+        }
+    }
+}
+}
+}
+*/
 std::string Carta::getNombreCarta(){
 	std::string nombreCarta;
 	switch(this->tipoCarta){
